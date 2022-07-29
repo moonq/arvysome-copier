f62e77f3c847f2d78f5885fce29d26d8250df25e
Ville Rantanen <ville.rantanen@reaktor.com>
2022-07-29 18:28:18 +0300

    add docs

--- arvysome-copier.py
+++ arvysome-copier.py
@@ -9,23 +9,28 @@ import argparse
 
 
 def get_options():
+    """ Reads user options, and returns as object """
 
+    # input folder, output folder, file of ids, file of acronyms
     print(options)
     return options
 
 
 def get_filelist(input_folder):
+    """ Recursively create list of all files with paths """
 
     # print(filelist)
     pass
 
 
 def read_csv():
+    """ Reads a file and returns its contents as a list """
     # print(read_data)
     pass
 
 
 def filter_filelist(all_files, ids, acronyms):
+    """ Removes non-matching entries from a file list """
 
     filtered = []
     for f in all_files:
@@ -36,13 +41,13 @@ def filter_filelist(all_files, ids, acronyms):
 
 
 def file_matches(fname, ids, acronyms):
-
+    """ Return True if file parses to predefined format, and matches found in IDs and acronyms """
     parsed = parse.parse("parsing-format", os.path.basename(fname))
     return parsed["id"] in ids and parsed["acronym"] in acronyms
 
 
 def copy_files(filtered_files, output_folder):
-
+    """ Copy list of files to a folder """
     if not os.path.exists(output_folder):
         os.mkdir(output_folder)
 
@@ -50,6 +55,7 @@ def copy_files(filtered_files, output_folder):
 
 
 def main():
+    """ Performs all the steps """
 
     options = get_options()
     # all_files = get_filelist(options.input)
